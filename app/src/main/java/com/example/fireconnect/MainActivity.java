724964fafc5c9e68f1563ba34002a9479b0adbc1
Yassin Chamlal <81307380+yassin-c@users.noreply.github.com>
2022-03-09 17:04:27 +0100

    Initial commit

---
+++ app/src/main/java/com/example/fireconnect/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.fireconnect;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
